bb668d6b89ec9c259b306355562209f97929608e
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2023-05-14 20:33:27 +0530

    Create ex5.py

---
+++ pythin/college_exercise/ex5.py
@@ -0,0 +1,19 @@
+#5. Write program to create and read from CSV file.
+import csv
+
+n = int(input("1.read \n2.appand\nenter :"))
+match n:
+    case 1:
+        #read
+        with open("new.csv","r") as file:
+            reader = csv.reader(file)
+            for row in reader :
+                print(row)
+    case 2:
+        #Write
+        with open("new.csv",'w',newline='') as file:
+            writer = csv.writer( file,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            k = int(input("this csv has 3 col ,enter : "))
+            for i in range(0,k):
+                arr= input("enter string separated by space: ")
+                writer.writerow(arr.split(" "))
