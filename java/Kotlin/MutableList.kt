c8ee5e08d944f2985d37b1ef76fcf4a836a832fe
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2023-05-17 16:24:48 +0530

    init

---
+++ java/Kotlin/MutableList.kt
@@ -0,0 +1,7 @@
+//only string or int can be const
+const i =5
+val mutability_cheak= mutableListOf(1,2,3,4,5,6)
+fun main(){
+    mutability_cheak.add(22)
+    println(mutability_cheak)
+}
--- java/Kotlin/abstraction.kt
+++ java/Kotlin/abstraction.kt
@@ -2,7 +2,7 @@ abstract class Person(){
     abstract fun det()
 }
 
-class Student(val name:String="unknown",val age:Int =0):Person(){
+class Student(val name:String="unknown",val age:Int =0): Person() {
     override fun det(){
         println("name : $name \nage : $age")
     }
